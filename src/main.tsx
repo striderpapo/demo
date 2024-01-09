f7d4bb10fa3ff6c2f2c8258413670f31ebddac9b
Arturo González <arturogarza.gonz@hotmail.com>
2024-01-09 13:20:40 -0600

    modificado main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
+//import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 //import Addpet from './main/containers/pruebapet.jsx'
 //import App from './App.tsx'
 import './assets/styles.css'
-import { BrowserRouter } from "react-router-dom";
+//import { BrowserRouter } from "react-router-dom";
 
-const createButton=(text:string)=>{
+/*const createButton=(text:string)=>{
   return(
     <button>{text}</button>
   )
@@ -16,7 +16,7 @@ const SpecialButton=({text})=>{
   return(
     <button>{text}</button>
   )
-}
+}*/
 ReactDOM.createRoot(document.getElementById('root')!).render(
   
   <App/>
