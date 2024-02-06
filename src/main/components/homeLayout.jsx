c09d519b9c2fa602d4b2b42d8e97128655d1beff
Arturo González <arturogarza.gonz@hotmail.com>
2024-02-06 15:40:41 -0600

    crud completo

--- src/main/components/homeLayout.jsx
+++ src/main/components/homeLayout.jsx
@@ -1,6 +1,14 @@
 import { Link,useNavigate,Outlet } from "react-router-dom";
+import { useState,useRef  } from 'react';
+import AgregarLayout from '../containers/agregar'
+import MostrarLayout from '../containers/mostrar'
+import ActualizarLayout from '../containers/actualizar'
+
 
 function HomeLayout(props){ 
+  const [tipo, setTipo] = useState(1);
+  //let valorSinRenderizado = useRef(0);
+  //console.log(valorSinRenderizado.current)
   console.log(props)
   const handleLogout = async () => {
     try {
@@ -10,10 +18,28 @@ function HomeLayout(props){
     }
   };
 
+  const changeTipo = (num) => {
+    //valorSinRenderizado.current=1
+    setTipo(num)
+   // console.log(valorSinRenderizado)
+  }
+
   return(
-    <div>
-    <h2>Este es elhome luego de logeo {props.nameUser}</h2>
-    <button onClick={handleLogout}>cerrar sesion</button>
+    <div className="style-home">
+      <div className="style-banner">
+      <h2>crud sencillo {props.nameUser}</h2>
+        <button onClick={handleLogout}>cerrar sesion</button>
+      </div>
+      <div className="crud-section">
+    <button className="buttons-design"  style={{ backgroundColor:tipo==1? "rgba(13,67,133,1)":"" }} onClick={() => changeTipo(1)}>Agregar</button>
+    <button className="buttons-design" style={{ backgroundColor:tipo==2? "rgba(13,67,133,1)":"" }}onClick={() => changeTipo(2)}>Mostrar</button>
+    <button className="buttons-design" style={{ backgroundColor:tipo==3? "rgba(13,67,133,1)":"" }}onClick={() => changeTipo(3)}>Actualizar</button>
+    <section>
+      {tipo==1?<AgregarLayout/>:null}
+      {tipo==2?<MostrarLayout/>:null}
+      {tipo==3?<ActualizarLayout/>:null}
+    </section>
+    </div>
     <Outlet />
   </div>
       
