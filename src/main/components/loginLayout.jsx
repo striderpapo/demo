fc17557bf3dda671f74e810b820370e2bb205309
Arturo González <arturogarza.gonz@hotmail.com>
2024-07-29 20:26:18 -0600

    boton face

--- src/main/components/loginLayout.jsx
+++ src/main/components/loginLayout.jsx
@@ -1,4 +1,5 @@
 import { Link,useNavigate,Outlet } from "react-router-dom";
+import FacebookLogin from 'react-facebook-login';
 import { useState } from 'react';
 import Menu from '../containers/menu'
 function LoginLayout(props){ 
@@ -45,6 +46,50 @@ function LoginLayout(props){
         navigate("/nosotros");
       }
 
+      const responseFacebook =  async (response) => {
+    const { accessToken } = response;
+    console.log(accessToken);
+    try {
+    const responseback =  await fetch('http://192.168.1.68:3700/api/sfuser', {
+      //const response = await fetch('https://backenddemosite.onrender.com/api/guser', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({accessToken}),
+      });
+        console.log(responseback.ok)
+        if(responseback.ok){
+          const data = await responseback.json();
+          console.log(data.usuario.token)
+          props.onLogin(data.usuario.token); // Llama a la función proporcionada por el padre
+        }else{
+          //setLoginError('Se proporcionó nombre de usuario o contraseña incorrectos.');
+        }
+      } catch (error) {
+        console.error('Error enviando los datos al backend:', error);
+      }
+    // Aquí puedes manejar el response para autenticar al usuario en tu sistema.
+    
+    /*fields="name,picture"   Solo solicitando nombre y foto de perfil
+        scope="public_profile"   Solo solicitando el perfil público
+        con esto solo pido nombre y foto*/
+  };
+
+    
+      /*const handleLoginFace = () => {
+        window.FB.login(response => {
+          if (response.authResponse) {
+            console.log('Bienvenido! Fetching your information.... ');
+            window.FB.api('/me', function(response) {
+              console.log('Good to see you, ' + response.name + '.');
+            });
+          } else {
+            console.log('User cancelled login or did not fully authorize.');
+          }
+        });
+      };*/
+
   return(
     <div className="login-div">
       <Menu/>
@@ -68,6 +113,16 @@ function LoginLayout(props){
     <button onClick={handleLogin} className="but-login">Iniciar Sesión</button>
     <button onClick={handleClick} className="but-registro">Registrarte</button>
     </div>
+    <div className="facebook-button-container">
+    <FacebookLogin
+        appId="406310499108431"
+        autoLoad={false}
+        fields="name,email,picture"
+        callback={responseFacebook}
+        icon="fa-facebook"
+        textButton="Iniciar sesión con Facebook" 
+      />
+      </div>
     {loginError && <p className="error-message">{loginError}</p>}
     </div>
   </div>
